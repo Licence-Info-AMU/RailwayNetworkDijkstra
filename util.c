bf53f9cd6875f567c9d33d0eccb08af58ad8a3bd
moonlight83340 <moonlight83340@users.noreply.github.com>
2017-01-30 22:26:18 +0100

    util.c et .h

---
+++ util.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char * skip_separator(char * line,char separator){    //magic thing are magic
+	for (int i = 0; i < strlen(line); ++i){
+		if (line[i]==separator){
+			return line+i+1;
+		}
+	}
+	return line;
+}
---
+++ util.h
@@ -0,0 +1,6 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+char * skip_separator(char * line,char separator);
+
+#endif
