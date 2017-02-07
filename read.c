28991f082a68dfef36d7c7c7274e10c30318a38a
moonlight83340 <moonlight83340@users.noreply.github.com>
2017-02-07 02:38:13 +0100

    Update read

--- read.c
+++ read.c
@@ -2,20 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include "read.h"
-#include "util.h"
 #include "trace.h"
-#include <assert.h>
 
 void read_file(char * filename ,RR *RRInstance){
 	RRInstance->nbville=0;
 	FILE *file = fopen ( filename, "r" );
 	
 	if ( file != NULL ){
-
 		char line [128];
-		// char *line =malloc(sizeof(char)*128);
-		// assert(line!=NULL);
-
 		if ( fgets (line, sizeof(line), file ) != NULL ){			//nombre de ville
 			RRInstance->nbville=atoi(line);
 			RRInstance->villes=malloc(sizeof(ville)* RRInstance->nbville );
@@ -23,22 +17,18 @@ void read_file(char * filename ,RR *RRInstance){
 		else{
 			trace ("Bad file",__FILE__,__LINE__ );
 		}
-
+		
 		for (int i = 0; i <RRInstance->nbville ; ++i)						//coordonnées des villes
 		{
 			if ( fgets ( line, sizeof(line), file ) != NULL ){
 				RRInstance->villes[i].x=atoi(strtok(line," "));					//version propre
 				RRInstance->villes[i].y=atoi(strtok(NULL," "));
-
-				// RRInstance->villes[i].x=atoi(line);							//version je fait a ma façon
-				// RRInstance->villes[i].y=atoi(skip_separator(line,' '));
 			}
 			else{
 				trace ("Bad file",__FILE__,__LINE__ );
 			}
-
 		}
-
+		
 		if ( fgets ( line, sizeof(line), file ) != NULL ){			//nombre de lignes
 			RRInstance->nbligne=atoi(line);
 			RRInstance->lignes=malloc(sizeof(ligne) * RRInstance->nbligne);
@@ -46,6 +36,7 @@ void read_file(char * filename ,RR *RRInstance){
 		else{
 			trace ("Bad file",__FILE__,__LINE__ );
 		}
+		
 		for (int nligne = 0; nligne < RRInstance->nbligne; ++nligne)
 		{
 			if(fgets ( line, sizeof(line), file ) != NULL){			//nombre de villes dans la première ligne
@@ -60,12 +51,6 @@ void read_file(char * filename ,RR *RRInstance){
 				for (int i = 1; i < RRInstance->lignes[nligne].nbvilles; ++i){
 					RRInstance->lignes[nligne].villes[i]=atoi(strtok(NULL," "));
 				}
-
-				// char *tmp=line;
-				// for (int i = 0; i < RRInstance->lignes[nligne].nbvilles; ++i){			//version je fait a ma façon
-				// 	RRInstance->lignes[nligne].villes[i]=atoi(tmp);
-				// 	tmp=skip_separator(tmp,' ');
-				// }
 			}
 			else{
 				trace ("Bad file",__FILE__,__LINE__ );
@@ -77,9 +62,7 @@ void read_file(char * filename ,RR *RRInstance){
 				for (int j = 0; j < RRInstance->lignes[nligne].nbvilles ; ++j)
 				{
 					RRInstance->lignes[nligne].horaires[j]=malloc(sizeof(int *) * RRInstance->lignes[nligne].nbhoraires);
-				}
-					
-				
+				}				
 			}
 			else{
 				trace ("Bad file",__FILE__,__LINE__ );
@@ -129,12 +112,4 @@ void show_RR(RR * RRInstance){
 			printf("\n");
 		}
 	}
-}
-
-int main (int argc , char * argv[]){
-	char filename[] = "test.txt";
-	struct RR RRInstance;
-	read_file(filename,&RRInstance);
-	show_RR(&RRInstance);
-	return 0;
 }
