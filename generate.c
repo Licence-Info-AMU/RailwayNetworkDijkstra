eea915157d52a1a64fa9f95906d39981c84e0c73
moonlight83340 <moonlight83340@users.noreply.github.com>
2017-02-07 02:40:26 +0100

    start generate

---
+++ generate.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "read.h"
+#include "trace.h"
+
+void genereReseau(int n, int d, int m, int max){
+
+}
---
+++ generate.h
@@ -0,0 +1,6 @@
+#ifndef GENERATE_H
+#define GENERATE_H
+
+void genereReseau(int n, int d, int m, int max);
+
+#endif
