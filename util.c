21831e2dbef0098d429358275b516a218bba4263
moonlight83340 <moonlight83340@users.noreply.github.com>
2017-02-07 02:36:45 +0100

    Util.c et Util.h swap

--- util.c
+++ util.c
@@ -10,3 +10,10 @@ char * skip_separator(char * line,char separator){    //magic thing are magic ri
 	}
 	return line;
 }
+
+void swap_tab_int(int * tab, int i, int j){
+	int temp;
+	temp = tab[i];
+	tab[i] = tab[j];
+	tab[j] = temp;
+}
--- util.h
+++ util.h
@@ -3,4 +3,6 @@
 
 char * skip_separator(char * line,char separator);
 
+void swap_tab_int(int * tab, int i, int j);
+
 #endif
